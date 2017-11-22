554790530c286a5453d9c488de68c2892c2cc6ec
Serj Ilyashenko <serj.ilyashenko@gmail.com>
2017-11-22 17:59:39 +0300

    categories prototype

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,226 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-class App extends React.Component {
-  state = {
-    world: 'hi',
+let idCounter = 10;
+
+class TextInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.value,
+      isEditing: false,
+    };
+  }
+
+  handleOnChange = e => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  handleOnDoubleClick = () => {
+    this.setState({
+      isEditing: true,
+    });
+  };
+
+  moveCaretAtEnd = e => {
+    const tempValue = e.target.value;
+    e.target.value = '';
+    e.target.value = tempValue;
+  };
+
+  handleOnSubmit = e => {
+    const {value} = this.state;
+    e.preventDefault();
+    if (value) {
+      this.props.onSave(value);
+      this.setState({
+        isEditing: false,
+      });
+    }
   };
 
   render() {
+    const {isEditing} = this.state;
+    const value = isEditing ? this.state.value : this.props.value;
+    if (!isEditing) {
+      return <span onDoubleClick={this.handleOnDoubleClick}>{this.props.value}</span>;
+    }
+
+    return (
+      <form
+        style={{display: 'inline-block'}}
+        onSubmit={this.handleOnSubmit}
+        onBlur={this.handleOnSubmit}
+      >
+        <input value={value} onChange={this.handleOnChange} onDoubleClick={this.handleOnDoubleClick}/>
+      </form>
+    );
+  }
+}
+
+class App extends Component {
+  state = {
+    newCategoryTitle: '',
+    categoriesById: {
+      1: {
+        id: 1,
+        title: 'hello world',
+        children: [],
+        parent: null,
+        tasks: []
+      },
+      2: {
+        id: 2,
+        title: 'test category',
+        children: [],
+        parent: null,
+        tasks: [1, 2]
+      }
+    },
+    rootCategories: [1, 2],
+    activeCategory: 2,
+    tasksById: {
+      1: {
+        id: 1,
+        title: 'task 1',
+        completed: false,
+        category: 2,
+      },
+      2: {
+        id: 2,
+        title: 'task 2',
+        completed: true,
+        category: 2
+      }
+    }
+  };
+
+  handleOnChange = e => {
+    this.setState({
+      newCategoryTitle: e.target.value,
+    });
+  };
+
+  handleOnSubmit = e => {
+    const {newCategoryTitle} = this.state;
+    e.preventDefault();
+    this.addCategory(newCategoryTitle);
+  };
+
+  addCategory(title, parentId) {
+    if (!title) {
+      return;
+    }
+
+    const id = idCounter++;
+    const category = {title, id, deleted: false, children: [], parent: null};
+    const rootCategories = this.state.rootCategories.slice();
+    let categoriesById = {...this.state.categoriesById};
+
+    if (parentId) {
+      const parent = categoriesById[parentId];
+      const children = parent.children.slice();
+      children.push(category.id);
+      categoriesById = {...categoriesById, [parent.id]: {...parent, children, parent: parent.id}};
+    } else {
+      rootCategories.push(category.id);
+    }
+
+    categoriesById = {...categoriesById, [category.id]: category};
+    this.setState({
+      newCategoryTitle: '',
+      rootCategories,
+      categoriesById,
+    });
+  }
+
+  removeCategory(id) {
+    this.setState(prevState => ({
+      ...prevState,
+      categoriesById: {
+        ...prevState.categoriesById,
+        [id]: {
+          ...prevState.categoriesById[id],
+          deleted: true,
+        },
+      },
+    }));
+  }
+
+  handleOnSave = (id, value) => {
+    this.setState(prevState => ({
+      ...prevState,
+      categoriesById: {
+        ...prevState.categoriesById,
+        [id]: {
+          ...prevState.categoriesById[id],
+          title: value,
+        },
+      },
+    }));
+  };
+
+  handleOnClick = id => {
+    this.setState({
+      activeCategory: id,
+    })
+  };
+
+  renderCategories(ids) {
+    const {activeCategory} = this.state;
+    return ids.map(id => {
+      const category = this.state.categoriesById[id];
+
+      if (category.deleted) {
+        return null;
+      }
+
+      const children = category.children.length ? this.renderCategories(category.children) : null;
+      return (
+        <div key={id} className="category-item">
+          <span onClick={() => this.handleOnClick(id)}>
+            {activeCategory === id ? '>' : ''}
+            <TextInput value={category.title} onSave={value => this.handleOnSave(id, value)}/>{' '}
+          </span>
+          <span onClick={() => this.addCategory('children name', id)}>+</span>{' '}
+          <span onClick={() => this.removeCategory(id)}>x</span>
+          <div>{children}</div>
+        </div>
+      );
+    });
+  }
+
+  renderTasks(ids = []) {
+    return ids.map(id => (
+      <div key={id}>hello {this.state.tasksById[id].title}</div>
+    ));
+  }
+
+  render() {
+    const {activeCategory, categoriesById, rootCategories, newCategoryTitle} = this.state;
+    const activeTasks = activeCategory ? categoriesById[activeCategory].tasks : [];
     return (
       <div>
-        <h1 className="app__element">
-          {this.props.text} {this.state.world}
-        </h1>
+        <form className="input-group" onSubmit={this.handleOnSubmit}>
+          <input
+            name="add-category"
+            type="text"
+            value={newCategoryTitle}
+            onChange={this.handleOnChange}
+            className="form-control"
+            placeholder="Enter category title"
+            aria-label="add-category"
+          />
+          <span className="input-group-btn">
+            <input type="submit" value="Add" className="btn btn-secondary"/>
+          </span>
+        </form>
+        <h2>Categories</h2>
+        <div>{this.renderCategories(rootCategories)}</div>
+        <h2>Tasks for {activeCategory}</h2>
+        <div>{this.renderTasks(activeTasks)}</div>
       </div>
     );
   }
--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -18,7 +18,17 @@ describe('components', () => {
   describe('App', () => {
     it('should render container', () => {
       const { wrapper } = setup();
-      expect(wrapper.find('.test-class_mod2')).toHaveLength(1);
+      // expect(wrapper.find('.test-class_mod2')).toHaveLength(1);
+    });
+    it('should add category item', () => {
+      const { wrapper } = setup();
+      expect(wrapper.find('.category-item')).toHaveLength(0);
+      wrapper.find('input[type="text"]').props().onChange({target: 'new text 1'});
+      wrapper.find('input[type="submit"]').simulate('submit');
+      expect(wrapper.find('.category-item')).toHaveLength(1);
+      wrapper.find('input[type="text"]').props().onChange({target: 'new text 3'});
+      wrapper.find('input[type="submit"]').simulate('submit');
+      expect(wrapper.find('.category-item')).toHaveLength(2);
     });
   });
 });
