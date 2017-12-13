7d3a70abd61d5d03d3cde212028ce55d1341b4b6
Serj Ilyashenko <serj.ilyashenko@gmail.com>
2017-12-13 22:17:01 +0300

    Links

--- src/components/CategoriesNavigation.jsx
+++ src/components/CategoriesNavigation.jsx
@@ -1,6 +1,24 @@
+/* eslint-disable */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AddRootCategory from './AddRootCategory';
+
+const Category = ({ id, children }) => (
+  <div className="tree-group">
+    <div className="tree-group__row">
+      <Link className="tree-group__row-link" to={`/categories/${id}`} href={`/categories/${id}`}>
+        <input name="" type="checkbox" checked onChange={() => {}} />
+        Category {id}
+      </Link>
+      <span className="glyphicon glyphicon__edit" />
+      <div className="tree-group__row-actions">
+        <span className="glyphicon glyphicon__delete" />
+        <span className="glyphicon glyphicon__add" />
+      </div>
+    </div>
+    <div className="tree-list">{children}</div>
+  </div>
+);
 
 const CategoriesNavigation = () => (
   <div className="scrollable-container">
@@ -9,126 +27,42 @@ const CategoriesNavigation = () => (
     </div>
     <div className="scrollable-container__content scrollable-container__content_left">
       <div className="tree">
-        <div className="tree-group">
-          <div className="tree-group__row">
-            <Link className="tree-group__row-link" to="/" href="/">
-              <input name="" type="checkbox" checked onChange={() => {}} />
-              Category 1
-            </Link>
-            <span className="glyphicon glyphicon__edit" />
-            <div className="tree-group__row-actions">
-              <span className="glyphicon glyphicon__delete" />
-              <span className="glyphicon glyphicon__add" />
-            </div>
-          </div>
-          <div className="tree-list">
-            <div className="tree-group">
-              <div className="tree-group__row">
-                <Link className="tree-group__row-link" to="/" href="/">
-                  <input name="" type="checkbox" onChange={() => {}} />
-                  Category 2 1
-                </Link>
-                <span className="glyphicon glyphicon__edit" />
-                <div className="tree-group__row-actions">
-                  <span className="glyphicon glyphicon__delete" />
-                  <span className="glyphicon glyphicon__add" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="tree-group">
-          <div className="tree-group__row">
-            <Link className="tree-group__row-link" to="/" href="/">
-              <input name="" type="checkbox" onChange={() => {}} />
-              Category 2
-            </Link>
-            <span className="glyphicon glyphicon__edit" />
-            <div className="tree-group__row-actions">
-              <span className="glyphicon glyphicon__delete" />
-              <span className="glyphicon glyphicon__add" />
-            </div>
-          </div>
-          <div className="tree-list">
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 1</div>
-            </div>
-          </div>
-        </div>
-        <div className="tree-group">
-          <div className="tree-group__row">Category 3</div>
-          <div className="tree-list">
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 1</div>
-            </div>
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 2</div>
-            </div>
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 3</div>
-            </div>
-          </div>
-        </div>
-        <div className="tree-group">
-          <div className="tree-group__row">Category 1</div>
-          <div className="tree-list">
-            <div className="tree-group">
-              <div className="tree-group__row">Category 1 1</div>
-            </div>
-          </div>
-        </div>
-        <div className="tree-group">
-          <div className="tree-group__row">Category 2</div>
-          <div className="tree-list">
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 1</div>
-            </div>
-          </div>
-        </div>
-        <div className="tree-group">
-          <div className="tree-group__row">Category 3</div>
-          <div className="tree-list">
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 1</div>
-            </div>
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 2</div>
-            </div>
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 3</div>
-            </div>
-          </div>
-        </div>
-        <div className="tree-group">
-          <div className="tree-group__row">Category 1</div>
-          <div className="tree-list">
-            <div className="tree-group">
-              <div className="tree-group__row">Category 1 1</div>
-            </div>
-          </div>
-        </div>
-        <div className="tree-group">
-          <div className="tree-group__row">Category 2</div>
-          <div className="tree-list">
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 1</div>
-            </div>
-          </div>
-        </div>
-        <div className="tree-group">
-          <div className="tree-group__row">Category 3</div>
-          <div className="tree-list">
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 1</div>
-            </div>
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 2</div>
-            </div>
-            <div className="tree-group">
-              <div className="tree-group__row">Category 3 3</div>
-            </div>
-          </div>
-        </div>
+        <Category id="1">
+          <Category id="1.1" />
+          <Category id="1.2" />
+        </Category>
+        <Category id="2">
+          <Category id="2.1" />
+        </Category>
+        <Category id="3">
+          <Category id="3.1" />
+          <Category id="3.2" />
+          <Category id="3.2" />
+        </Category>
+        <Category id="1">
+          <Category id="1.1" />
+          <Category id="1.2" />
+        </Category>
+        <Category id="2">
+          <Category id="2.1" />
+        </Category>
+        <Category id="3">
+          <Category id="3.1" />
+          <Category id="3.2" />
+          <Category id="3.2" />
+        </Category>
+        <Category id="1">
+          <Category id="1.1" />
+          <Category id="1.2" />
+        </Category>
+        <Category id="2">
+          <Category id="2.1" />
+        </Category>
+        <Category id="3">
+          <Category id="3.1" />
+          <Category id="3.2" />
+          <Category id="3.2" />
+        </Category>
       </div>
     </div>
   </div>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,75 +1,49 @@
+/* eslint-disable */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const hi = '';
 
-const TaskList = () => (
-  <div className="scrollable-container">
-    <div className="scrollable-container__header">
-      <div className="adding-input adding-input_right">
-        <form className="input-group" onSubmit={() => {}}>
-          <input
-            name="add-category"
-            type="text"
-            value={hi}
-            onChange={() => {}}
-            className="form-control"
-            placeholder="Enter task title"
-            aria-label="add-category"
-          />
-          <span className="input-group-btn">
-            <input type="submit" value="Add" className="btn btn-secondary" />
-          </span>
-        </form>
-      </div>
-    </div>
-    <div className="scrollable-container__content scrollable-container__content_right">
-      <div className="task-item">
-        <span className="task-item__title">
-          <input name="" type="checkbox" checked onChange={() => {}} />
-          Todo Item #1
-        </span>
-        <div className="task-item__actions">
-          <span className="glyphicon glyphicon__edit" />
-        </div>
-      </div>
-      <div className="task-item">
-        <span className="task-item__title">
-          <input name="" type="checkbox" checked onChange={() => {}} />
-          Todo Item #2
-        </span>
-        <div className="task-item__actions">
-          <span className="glyphicon glyphicon__edit" />
-        </div>
-      </div>
-      <div className="task-item">
-        <span className="task-item__title">
-          <input name="" type="checkbox" checked onChange={() => {}} />
-          Todo Item #3
-        </span>
-        <div className="task-item__actions">
-          <span className="glyphicon glyphicon__edit" />
-        </div>
-      </div>
-      <div className="task-item">
-        <span className="task-item__title">
-          <input name="" type="checkbox" checked onChange={() => {}} />
-          Todo Item #4
-        </span>
-        <div className="task-item__actions">
-          <span className="glyphicon glyphicon__edit" />
-        </div>
-      </div>
-      <div className="task-item">
-        <span className="task-item__title">
-          <input name="" type="checkbox" checked onChange={() => {}} />
-          Todo Item #5
-        </span>
-        <div className="task-item__actions">
-          <span className="glyphicon glyphicon__edit" />
-        </div>
-      </div>
+const Task = ({ id }) => (
+  <div className="task-item">
+    <Link to={`/tasks/${id}`} href={`/tasks/${id}`} className="task-item__title">
+      <input name="" type="checkbox" checked onChange={() => {}} />
+      Todo Item #{id}
+    </Link>
+    <div className="task-item__actions">
+      <span className="glyphicon glyphicon__edit" />
     </div>
   </div>
 );
+
+const TaskList = () => {
+  const taskElements = new Array(20).fill(0).map((it, index) => <Task id={index} key={index} />);
+
+  return (
+    <div className="scrollable-container">
+      <div className="scrollable-container__header">
+        <div className="adding-input adding-input_right">
+          <form className="input-group" onSubmit={() => {}}>
+            <input
+              name="add-category"
+              type="text"
+              value={hi}
+              onChange={() => {}}
+              className="form-control"
+              placeholder="Enter task title"
+              aria-label="add-category"
+            />
+            <span className="input-group-btn">
+              <input type="submit" value="Add" className="btn btn-secondary" />
+            </span>
+          </form>
+        </div>
+      </div>
+      <div className="scrollable-container__content scrollable-container__content_right">
+        {taskElements}
+      </div>
+    </div>
+  );
+};
 
 export default TaskList;
